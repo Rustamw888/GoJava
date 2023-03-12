2cb1a0b3a0f361c6448d2d25b4b1be2bb917f2ee
qaMilord <rustamw888@gmail.com>
2023-03-13 00:46:23 +0300

    Lesson 12.2

---
+++ src/main/java/Lesson_12/StringTest.java
@@ -0,0 +1,11 @@
+package Lesson_12;
+
+public class StringTest {
+
+  public static void main(String[] args) {
+    String str = "Hello";
+    String str2 = str.substring(1, 5);
+    System.out.println(str2);
+  }
+
+}
