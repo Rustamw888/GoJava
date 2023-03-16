8c4d945d6c0e126b95dcadfb7d1a77563f9e438d
qaMilord <rustamw888@gmail.com>
2023-03-17 02:27:02 +0300

    Lesson 15_5

--- src/main/java/Lesson_15/MyRegex.java
+++ src/main/java/Lesson_15/MyRegex.java
@@ -17,12 +17,27 @@ public class MyRegex {
     }
   }
 
-    public static void main(String[] args) {
+    public static void main2(String[] args) {
     String s1 = "арапр dJavaSE the bestJavaScript Java!";
     Pattern pattern = Pattern.compile("Java\\w.");
     Matcher matcher = pattern.matcher(s1);
     while (matcher.find()) {
       System.out.println(matcher.group());
+    }
+  }
+
+    public static void main(String[] args) {
+    String text = "java-SE_2023@mail.com";
+    Pattern pattern = Pattern.compile("[A-Za-z0-9]{1,}[\\-]{1}[_]{0,1}[A-Za-z0-9]{1,}+@([A-Za-z]{1,}+[\\.]{1}[a-z]{2,})");
+    Pattern pattern2 = Pattern.compile("[A-z0-9]{1,}[\\-]{1}[_]{0,1}[A-z0-9]{1,}+@([A-z]{1,}+[\\.]{1}[a-z]{2,})");
+    Pattern pattern3 = Pattern.compile("(\\w+[\\-]{0,1}[_]{0,1}\\w+)+@(\\w+[\\.]{1}\\w{2,})");
+
+    Matcher matcher = pattern.matcher(text);
+    boolean result = matcher.matches();
+    if (result) {
+      System.out.println(text);
+    } else {
+      System.out.println("Не найдено");
     }
   }
 }
