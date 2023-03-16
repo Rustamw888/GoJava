2ec74cf9b0f2744da00561a1f21f5c09bcb4ba5d
qaMilord <rustamw888@gmail.com>
2023-03-17 02:04:15 +0300

    Lesson 15_4

--- src/main/java/Lesson_15/MyRegex.java
+++ src/main/java/Lesson_15/MyRegex.java
@@ -5,15 +5,24 @@ import java.util.regex.Pattern;
 
 public class MyRegex {
 
-  public static void main(String[] args) {
-    String s1 = "арапр Java the best";
-    Pattern pattern = Pattern.compile(".*\\bJava\\b.*");
+  public static void main1(String[] args) {
+    String s1 = "арапр dJavaSE the bestJavaScript Java!";
+    Pattern pattern = Pattern.compile(".*\\wJava\\w.*");
     Matcher matcher = pattern.matcher(s1);
     boolean b = matcher.matches();
     if (b) {
       System.out.println("Да");
     } else {
       System.out.println("Нет");
+    }
+  }
+
+    public static void main(String[] args) {
+    String s1 = "арапр dJavaSE the bestJavaScript Java!";
+    Pattern pattern = Pattern.compile("Java\\w.");
+    Matcher matcher = pattern.matcher(s1);
+    while (matcher.find()) {
+      System.out.println(matcher.group());
     }
   }
 }
