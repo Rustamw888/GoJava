85e8f1229ad00e5178cba23b9951059e61c6781d
qaMilord <rustamw888@gmail.com>
2023-03-17 03:06:02 +0300

    Lesson 15_6

--- src/main/java/Lesson_15/MyRegex.java
+++ src/main/java/Lesson_15/MyRegex.java
@@ -26,7 +26,7 @@ public class MyRegex {
     }
   }
 
-    public static void main(String[] args) {
+    public static void main3(String[] args) {
     String text = "java-SE_2023@mail.com";
     Pattern pattern = Pattern.compile("[A-Za-z0-9]{1,}[\\-]{1}[_]{0,1}[A-Za-z0-9]{1,}+@([A-Za-z]{1,}+[\\.]{1}[a-z]{2,})");
     Pattern pattern2 = Pattern.compile("[A-z0-9]{1,}[\\-]{1}[_]{0,1}[A-z0-9]{1,}+@([A-z]{1,}+[\\.]{1}[a-z]{2,})");
@@ -39,5 +39,44 @@ public class MyRegex {
     } else {
       System.out.println("Не найдено");
     }
+  }
+
+  public static void main4(String[] args) {
+    String text1 = "\u00A7Hello world! Hi world Bye world!!";
+
+    Pattern pattern = Pattern.compile("world");
+    Matcher matcher = pattern.matcher(text1);
+    String text2 = matcher.replaceAll("Java");
+    System.out.println(text1);
+    System.out.println(text2);
+  }
+
+  public static void main5(String[] args) {
+    String password = "123dgds";
+
+    System.out.println(password.matches("[0-9A-z]{6,}"));
+
+  }
+
+  public static void main6(String[] args) {
+    String password = "123dgds";
+
+    Pattern pattern = Pattern.compile("[0-9A-z]{6,}");
+    Matcher matcher = pattern.matcher(password);
+    System.out.println(matcher.matches());
+
+  }
+
+  public static void main(String[] args) {
+
+    System.out.println(passwordValid("123"));
+    System.out.println(passwordValid("dggf0250"));
+    System.out.println(passwordValid("%&*^fhfgh6516"));
+
+  }
+
+  public static boolean passwordValid(String password) {
+//    return password.matches("[0-9A-z]{6,}");
+    return password.matches(".*");
   }
 }
