3b0622303fccb6c6dbce6e650461004c735b143e
qaMilord <rustamw888@gmail.com>
2023-03-18 02:54:26 +0300

    Lesson 17_3

--- src/main/java/Lesson_17/FileClass.java
+++ src/main/java/Lesson_17/FileClass.java
@@ -1,7 +1,9 @@
 package Lesson_17;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Arrays;
 
 public class FileClass {
@@ -26,7 +28,7 @@ public class FileClass {
     }
   }
 
-  public static void main(String[] args) {
+  public static void main2(String[] args) {
     String path = "C:\\ProjectsJob\\GoJava\\src\\main\\java\\Lesson_17\\folder1\\folder1_1";
 
     File file = new File(path);
@@ -36,6 +38,43 @@ public class FileClass {
     } else {
       System.out.println("Папка в папке уже есть");
     }
-    System.out.println(Arrays.toString(file.list()));
+    String[] files = file.list();
+    System.out.println(Arrays.toString(files));
+
+    for (String s : files) {
+      File f = new File(path + "\\" + s);
+      System.out.println(f.isDirectory() ? (s + " is directory"): (s + " is file"));
+    }
+  }
+
+  public static void main(String[] args) throws IOException {
+
+    String path = "C:\\ProjectsJob\\GoJava\\src\\main\\java\\Lesson_17\\folder1\\folder1_1\\text1.txt";
+    InputStream inputStream = null;
+
+    try {
+      inputStream = new FileInputStream(path);
+      while (true) {
+
+        int iByte = inputStream.read();
+        if (iByte != -1) {
+          System.out.print((char)iByte);
+        } else {
+          System.out.println("\nEND");
+          break;
+        }
+
+      }
+    } catch (Exception e) {
+      throw new IOException("ERROR" + e + path);
+    } finally {
+      if (inputStream != null) {
+        try {
+          inputStream.close();
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      }
+    }
   }
 }
