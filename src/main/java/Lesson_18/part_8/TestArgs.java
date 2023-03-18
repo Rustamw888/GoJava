734fff164df85152df65b77df58a44326f75b9a8
qaMilord <rustamw888@gmail.com>
2023-03-18 22:44:04 +0300

    Lesson_18_8

--- src/main/java/Lesson_18/part_8/TestArgs.java
+++ src/main/java/Lesson_18/part_8/TestArgs.java
@@ -1,2 +1,12 @@
-package Lesson_18.part_8;public class TestArgs {
+package Lesson_18.part_8;
+
+public class TestArgs {
+
+  public static void main(String[] args) {
+    int number = Integer.parseInt(args[0]);
+
+    System.out.println(number);
+    System.out.println(number + 1);
+  }
+
 }
