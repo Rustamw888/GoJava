9829937f80babd660876c0e93fed10cc88e29ee1
qaMilord <rustamw888@gmail.com>
2023-03-19 23:53:52 +0300

    Lesson 19_4/5

---
+++ src/main/java/Lesson_19/part_3/DynamicArray.java
@@ -0,0 +1,28 @@
+package Lesson_19.part_3;
+
+public class DynamicArray {
+  int[] data = {0, 1, 2, 3, 4, 5, 6, 7};
+
+  public void add(int element, int index) {
+    int[] temp = new int[data.length + 1];
+    System.arraycopy(data, 0, temp, 0, index);
+    System.arraycopy(data, index, temp, index + 1, data.length - index);
+    temp[index] = element;
+    this.data = temp;
+  }
+
+  public void delete(int index) {
+    int[] temp = new int[data.length - 1];
+    System.arraycopy(data, 0, temp, 0, index);
+    System.arraycopy(data, index + 1, temp, index, data.length - index - 1);
+    this.data = temp;
+  }
+
+  public void add(int element) {
+    add(element, data.length);
+  }
+
+  public void delete() {
+    delete(data.length - 1);
+  }
+}
--- src/main/java/Lesson_19/part_3/TestArray.java
+++ src/main/java/Lesson_19/part_3/TestArray.java
@@ -9,22 +9,12 @@ public class TestArray {
     System.out.println(Arrays.toString(dynamicArray.data));
     dynamicArray.add(90, 4);
     System.out.println(Arrays.toString(dynamicArray.data));
+    dynamicArray.delete(8);
+    System.out.println(Arrays.toString(dynamicArray.data));
+    dynamicArray.add(85);
+    System.out.println(Arrays.toString(dynamicArray.data));
+    dynamicArray.delete();
+    System.out.println(Arrays.toString(dynamicArray.data));
   }
 
-
 }
-
-class DynamicArray {
-  int[] data = {0, 1, 2, 3, 4, 5, 6, 7};
-
-  public void add(int element, int index) {
-
-    int[] temp = new int[data.length + 1];
-    System.arraycopy(data, 0, temp, 0, index);
-    System.arraycopy(data, index, temp, index + 1, data.length - index);
-    temp[index] = element;
-    this.data = temp;
-  }
-}
-
-
