ebf795f1a35c12227189b4b38016bb1404a3c3c4
qaMilord <rustamw888@gmail.com>
2023-03-22 00:29:50 +0300

    Lesson 21_3

--- src/main/java/Lesson_21/part_2/Program.java
+++ src/main/java/Lesson_21/part_2/Program.java
@@ -5,11 +5,11 @@ public class Program {
   public static void main(String[] args) {
 
     PizzaStore store = new PizzaStore();
-    Pizza pizza1 = store.createPizza("cheese");
+    Pizza pizza1 = store.createPizza("NY", "NYcheese");
     pizza1.cut();
-    Pizza pizza2 = store.createPizza("pepperoni");
+    Pizza pizza2 = store.createPizza("CH", "CHpepperoni");
     pizza2.cut();
-    Pizza pizza3 = store.createPizza("vegan");
+    Pizza pizza3 = store.createPizza("VG", "vegan");
     pizza1.printInfo();
     pizza2.printInfo();
   }
@@ -24,20 +24,35 @@ abstract class Pizza {
 }
 
 class PizzaStore {
-  public Pizza createPizza(String type) {
-    if (type.equals("cheese")) {
-      return new CheesePizza();
-    } else if (type.equals("pepperoni")) {
-      return new PepperoniPizza();
+  public Pizza createPizza(String style, String type) {
+    if(style.equals("NY")) {
+      if (type.equals("NYcheese")) {
+        return new NYCheesePizza();
+      } else if (type.equals("NYpepperoni")) {
+        return new NYPepperoniPizza();
+      } else {
+        System.out.println("ERROR");
+        return null;
+      }
+    } else if (style.equals("CH")) {
+      if (type.equals("CHcheese")) {
+        return new CHCheesePizza();
+      } else if (type.equals("CHpepperoni")) {
+        return new CHPepperoniPizza();
+      } else {
+        System.out.println("ERROR");
+        return null;
+      }
     } else {
       System.out.println("ERROR");
       return null;
     }
+
   }
 }
 
-class CheesePizza extends Pizza {
-  private String name = "CheesePizza";
+class NYCheesePizza extends Pizza {
+  private String name = "NYCheesePizza";
   @Override
   public void printInfo() {
     System.out.println(name);
@@ -49,8 +64,29 @@ class CheesePizza extends Pizza {
   }
 }
 
-class PepperoniPizza extends Pizza {
-  private String name = "PepperoniPizza";
+class NYPepperoniPizza extends Pizza {
+  private String name = "NYPepperoniPizza";
+  @Override
+  public void printInfo() {
+    System.out.println(name);
+  }
+}
+
+class CHCheesePizza extends Pizza {
+  private String name = "CHCheesePizza";
+  @Override
+  public void printInfo() {
+    System.out.println(name);
+  }
+
+  @Override
+  void cut() {
+    System.out.println("Pizza does cut");
+  }
+}
+
+class CHPepperoniPizza extends Pizza {
+  private String name = "CHPepperoniPizza";
   @Override
   public void printInfo() {
     System.out.println(name);
