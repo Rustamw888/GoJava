277a6f4aca9a1e37e4204541cab089bc392d93c9
qaMilord <rustamw888@gmail.com>
2023-03-23 23:45:38 +0300

    Lesson 24_1

--- src/main/java/Lesson_24/part_1/Singleton.java
+++ src/main/java/Lesson_24/part_1/Singleton.java
@@ -4,6 +4,52 @@ public class Singleton {
 
   public static void main(String[] args) {
 
+    DataBase dataBase1 = DataBase.getInstance();
+    DataBase dataBase2 = DataBase.getInstance();
+    System.out.println("//////////***********///////////");
+    System.out.println(dataBase1.equals(dataBase2));
+    System.out.println(dataBase1.getName());
+    System.out.println(dataBase2.getName());
+    dataBase1.setName("Java");
+    System.out.println(dataBase1.getName());
+    System.out.println(dataBase2.getName());
+    System.out.println(dataBase1.equals(dataBase2));
+    System.out.println("//////////***********///////////");
   }
 
+}
+
+
+class DataBase {
+
+  private static String name = "Oracle";
+  private static DataBase base;
+
+  private DataBase() {}
+
+  public static DataBase getInstance() {
+    if (base == null) {
+      base = new DataBase();
+      return base;
+//      return new DataBase();
+    } else {
+      return base;
+    }
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public DataBase getBase() {
+    return base;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public void setBase(DataBase base) {
+    this.base = base;
+  }
 }
