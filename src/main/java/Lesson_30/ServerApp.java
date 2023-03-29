74f484e1009871fdd92508c5d8f1f8374c3d5db8
qaMilord <rustamw888@gmail.com>
2023-03-29 04:19:55 +0300

    Lesson 30_1

---
+++ src/main/java/Lesson_30/ServerApp.java
@@ -0,0 +1,38 @@
+package Lesson_30;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ServerApp {
+
+    public static void main(String[] args) {
+        try (ServerSocket server = new ServerSocket(8080)) {
+
+            System.out.println("Server started...");
+            while (true) {
+                try (Socket socket = server.accept();
+                     BufferedReader reader =
+                             new BufferedReader(
+                                     new InputStreamReader(socket.getInputStream()));
+                     BufferedWriter writer =
+                             new BufferedWriter(
+                                     new OutputStreamWriter(socket.getOutputStream()));
+                ) {
+                    System.out.println("Client connected");
+                    String request = reader.readLine();
+                    String response = String.format("Hello, your request = %s", request);
+
+                    writer.write(response);
+                    writer.newLine();
+                    writer.flush();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
