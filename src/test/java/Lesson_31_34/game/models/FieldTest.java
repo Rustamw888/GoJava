de1e2d8cf554e65407412689407833ae0619e99f
qaMilord <rustamw888@gmail.com>
2023-03-31 02:14:43 +0300

    Lesson 33

---
+++ src/test/java/Lesson_31_34/game/models/FieldTest.java
@@ -0,0 +1,49 @@
+package Lesson_31_34.game.models;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class FieldTest {
+
+  @Test
+  void setStateField() {
+    Field field = new Field(3);
+    String[][] state = field.getStateField();
+    String checkedValue = " ";
+    String realValue = state[0][0];
+
+    assertEquals(checkedValue, realValue);
+  }
+
+  @Test
+  void getCellField() {
+    Field field = new Field(3);
+    String[][] state = field.getStateField();
+    String checkedValue = " ";
+    field.setCellField(0, 0, checkedValue);
+    String realValue = state[0][0];
+
+    assertEquals(checkedValue, realValue);
+  }
+
+  @Test
+  void getStateField() {
+  }
+
+  @Test
+  void getSIZE_FIELD() {
+  }
+
+  @Test
+  void getDEFAULT_SYMBOL() {
+  }
+
+  @Test
+  void getMIN_NUMBER() {
+  }
+
+  @Test
+  void getMAX_NUMBER() {
+  }
+}
