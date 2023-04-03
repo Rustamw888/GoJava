1533b9b00a2f20b459cd66d2758b0a856749fbff
qaMilord <rustamw888@gmail.com>
2023-04-04 00:37:33 +0300

    Lesson 41_5

--- src/main/java/Lesson_41/part_5/StringTokenizerTest.java
+++ src/main/java/Lesson_41/part_5/StringTokenizerTest.java
@@ -1,11 +1,12 @@
 package Lesson_41.part_5;
 
 import java.io.*;
+import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class StringTokenizerTest {
 
-  public static void main(String[] args) {
+  public static void main1(String[] args) {
 
     File file;
     InputStreamReader isr = null;
@@ -34,6 +35,42 @@ public class StringTokenizerTest {
       } catch (IOException e) {
         throw new RuntimeException(e);
       }
+    }
+  }
+
+  public static void main2(String[] args) {
+
+    File file = new File("src/main/java/Lesson_41/part_5/test.csv");
+
+    try {
+      Scanner scanner = new Scanner(file);
+      StringTokenizer tokenizer = new StringTokenizer(scanner.nextLine(), ",");
+      System.out.println(tokenizer.countTokens());
+
+      while (tokenizer.hasMoreTokens()) {
+        System.out.println(tokenizer.nextToken());
+      }
+
+    } catch (FileNotFoundException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  public static void main(String[] args) {
+
+    File file = new File("src/main/java/Lesson_41/part_5/test.csv");
+
+    try {
+      Scanner scanner = new Scanner(file);
+      String string = scanner.nextLine();
+      String[] strings = string.split(",");
+
+      for (String string1 : strings) {
+        System.out.println(string1);
+      }
+
+    } catch (FileNotFoundException e) {
+      throw new RuntimeException(e);
     }
   }
 }
