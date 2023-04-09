298052d170d8dc6cbef5223dc3a30bedae2e6e4f
qaMilord <rustamw888@gmail.com>
2023-04-10 00:48:23 +0300

    Lesson 49_1 Add radio and Player classes

---
+++ src/main/java/Lesson_49/part_1/Main.java
@@ -0,0 +1,32 @@
+package Lesson_49.part_1;
+
+public class Main {
+
+  public static void main(String[] args) {
+
+  }
+}
+
+class Radio {
+  void onRadio() {
+    System.out.println("ON RADIO");
+  }
+  void offRadio() {
+    System.out.println("OFF RADIO");
+  }
+}
+
+class Player {
+  void onPlayer() {
+    System.out.println("ON Player");
+  }
+  void offPlayer() {
+    System.out.println("OFF Player");
+  }
+  void searchContent() {
+    System.out.println("search......");
+  }
+  void playContent() {
+    System.out.println("play");
+  }
+}
