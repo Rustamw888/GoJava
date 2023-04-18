1eb076586a3b5f62d897f3a2ff32f2443ae227c9
qaMilord <rustamw888@gmail.com>
2023-04-18 20:07:14 +0300

    Lesson 51_4

---
+++ src/main/java/Lesson_51/part_4/Users.java
@@ -0,0 +1,66 @@
+package Lesson_51.part_4;
+
+import java.util.Objects;
+
+public class Users {
+
+  private int id;
+  private String name;
+  private int age;
+  private String email;
+
+  public Users() {
+  }
+
+  public Users(int id, String name, int age, String email) {
+    this.id = id;
+    this.name = name;
+    this.age = age;
+    this.email = email;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public int getAge() {
+    return age;
+  }
+
+  public void setAge(int age) {
+    this.age = age;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    Users users = (Users) o;
+    return id == users.id && age == users.age && name.equals(users.name) && email.equals(users.email);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id, name, age, email);
+  }
+}
